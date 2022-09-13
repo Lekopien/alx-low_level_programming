fc15ab21a4e71a922efc5a15c921e5e89151136d
Lekopien <mavericklekopien@gmail.com>
2022-09-13 22:45:52 +0300

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,17 @@
  *
  * Description: Prints _putchar with _putchar
  *
- * Return:always 0
+ * Return: always 0
  */
+
 int main(void)
 {
 	int i = 0;
 	char c;
 	char s[] = "_putchar\n";
 
-	while (i < = 9)
+
+	while (i <= 9)
 	{
 		c = s[i];
 		_putchar(c);
