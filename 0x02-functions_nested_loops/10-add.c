7e7009a85d0ddb59f4433de81725430d1af62d06
Lekopien <mavericklekopien@gmail.com>
2022-09-15 00:33:06 +0300

    c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -adds two intergers
+ * @a: first integer
+ * @b: second interger
+ *
+ * Return: addition of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
