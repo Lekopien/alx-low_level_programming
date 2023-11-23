c2f97177316ced02814a6253884d27ae56a43938
Lekopien <mavericklekopien@gmail.com>
2023-11-23 01:53:52 -0800

    :art:improved file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>void *malloc_checked(unsigned int b)
+{
+	void *n;
+
+	n = malloc(b);
+
+	if (n == NULL)
+		exit(98);
+	return (n);
+}
